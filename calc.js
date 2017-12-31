7e0563e82bdb94964ab567aa017d5f80acd9e455
Nick Jacobson <33440907+nick-jac@users.noreply.github.com>
2017-12-31 16:49:50 +1300

    redone code nearly all good

--- calc.js
+++ calc.js
@@ -1,49 +1,36 @@
-$(document).ready(function(){
-	var testNumLength = function(number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Err");
-            }
-        } 
-    };
-	var number = "";
-    var newnumber = "";
-    var operator = "";
-    var totaldiv = $("#total");
-    totaldiv.text("0");
-    $("#numbers > a").not("#clear,#clearall").click(function(){
-		number += $(this).text();
-		totaldiv.text(number);
-		testNumLength(number);
-    });
-    $("#operators > a").not("#equals").click(function(){
-		operator = $(this).text();
-		newnumber = number;
-		number = "";
-		totaldiv.text("0");
-    });
-    $("#clear,#clearall").click(function(){
-		number = "";
-		totaldiv.text("0");
-		if ($(this).attr("id") === "clearall") {
-			newnumber = "";
-		}
-    });
-    $("#equals").click(function(){
-               if (operator === "+") {
-            number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-        } else if (operator === "-") {    
-            number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-        } else if (operator === "/") {
-            number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-        } else if (operator === "*") {
-        }   number = (parseInt(number, 10) + parseInt(newnumber,10)).tostring(10);
-        totaldiv.text(number);
-        testnumlength(number);
-        number = "";
-        newnumber = "";
-        
-    });
-});
+
+function btnplus() {
+    document.Calculator.display.value +="+";
+    document.Calculator.display.style.textAlign="right";
+}
+
+function btnsub() {
+    document.Calculator.display.value +="-";
+    document.Calculator.display.style.textAlign="right";
+}
+
+function btnmult() {
+    document.Calculator.display.value +="*";
+    document.Calculator.display.style.textAlign="right";
+}
+
+function btnmod() {
+    document.Calculator.display.value +="%";
+    document.Calculator.display.style.textAlign="right";
+}
+
+function btndot() {
+    document.Calculator.display.value +=".";
+    document.Calculator.display.style.textAlign="right";
+}
+
+function btndiv() {
+    document.Calculator.display.value +="/";
+    document.Calculator.display.style.textAlign="right";
+}
+
+function btnclear() {
+    document.Calculator.display.value ="";
+    document.Calculator.display.style.textAlign="right";
+}
+
